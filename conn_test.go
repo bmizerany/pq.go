0e6fcd9b89432e649709ca676ee419e1a83fb32b
Blake Mizerany <blake.mizerany@gmail.com>
2011-10-26 14:23:45 -0700

    startup complete

--- conn_test.go
+++ conn_test.go
@@ -3,6 +3,7 @@ package pq
 import (
 	"github.com/bmizerany/assert"
 	"net"
+	"os"
 	"testing"
 )
 
@@ -15,4 +16,15 @@ func TestConnStartupErr(t *testing.T) {
 
 	err = cn.Startup(Values{})
 	assert.NotEqual(t, nil, err)
+}
+
+func TestConnStartup(t *testing.T) {
+	nc, err := net.Dial("tcp", "localhost:5432")
+	assert.Equalf(t, nil, err, "%v", err)
+
+	cn := New(nc)
+	assert.Equalf(t, nil, err, "%v", err)
+
+	err = cn.Startup(Values{"user": os.Getenv("USER")})
+	assert.Equalf(t, nil, err, "%v", err)
 }
