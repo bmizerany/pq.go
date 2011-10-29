f014414b0b5d0dbc716e9d249e46b098eba65193
Blake Mizerany <blake.mizerany@gmail.com>
2011-10-28 22:52:30 -0700

    accept int as param

--- encode.go
+++ encode.go
@@ -9,8 +9,8 @@ func encodeParams(params []interface{}) (r []string) {
 	for i, param := range params {
 		switch param.(type) {
 		default:
-			panic(fmt.Sprintf("unknown type for %T", params))
-		case uint8, uint16, uint32, uint64, int8, int16, int32, int64:
+			panic(fmt.Sprintf("unknown type for %T", param))
+		case int, uint8, uint16, uint32, uint64, int8, int16, int32, int64:
 			r[i] = fmt.Sprintf("%d", param)
 		case string, []byte:
 			r[i] = fmt.Sprintf("%s", param)
